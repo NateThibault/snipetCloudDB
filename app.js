7c976ff35e8a4c2173c9e2974e9e62b3bfbd132c
NateThibault <ca4ever122@gmail.com>
2023-09-09 12:54:08 -0400

    correction app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const cors = require("cors");
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser');
 require('dotenv').config()
